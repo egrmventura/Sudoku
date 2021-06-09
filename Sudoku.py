73ef590aab11a4e74803c721d7869e48b388e02e
Matthew Ventura <matthew.t.ventura@gmail.com>
2021-06-08 21:41:59 -0400

    Cube Update

--- Sudoku.py
+++ Sudoku.py
@@ -3,9 +3,11 @@ import time
 import pygame as pg
 
 class Cube:
-    def __init__(self, row, col, val, permanent_val):
+    def __init__(self, row, col, width, height, val, permanent_val):
         self.row = row
         self.col = col
+        self.width = width
+        self.height = height
         self.permanent_val = permanent_val
         if permanent_val != 0:
             self.val = permanent_val
